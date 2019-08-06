9b236b7d21211589cf1c5bdfc56f0b42f21938a8
zatonskayay <zatonskaya_yulka@mail.ru>
2019-08-06 15:35:32 +0300

    Добавили принт при старте приложения

---
+++ IOSSummerExampleProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  IOSSummerExampleProject
+//
+//  Created by Yulia Zatonskaya on 06/08/2019.
+//  Copyright © 2019 Zatonskayay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello world!")
+        
+    }
+
+
+}
+
